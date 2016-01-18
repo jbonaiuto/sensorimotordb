b1f3d33632a7b75fbb47393c3a1446500962a200
jbonaiuto <jbonaiuto@gmail.com>
2016-01-18 16:55:45 +0000

    fixed data path

--- mirrordb/management/commands/import.py
+++ mirrordb/management/commands/import.py
@@ -7,7 +7,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         remove_all()
-        import_kraskov_data(os.path.join(settings.PROJECT_PATH,'data/kraskov/units4BODB.mat'),
-            os.path.join(settings.PROJECT_PATH,'data/kraskov/'))
-        sed=import_bonini_data([os.path.join(settings.PROJECT_PATH,'data/bonini/01_PIC_F5_09022012_mot_mirror_mrgSORTED.nex'),
-                                os.path.join(settings.PROJECT_PATH,'data/bonini/02_Pic_F5_10022012_mot_mirror_mrgSORTED.nex')])
+        import_kraskov_data(os.path.join(settings.PROJECT_PATH,'../data/kraskov/units4BODB.mat'),
+            os.path.join(settings.PROJECT_PATH,'../data/kraskov/'))
+        sed=import_bonini_data([os.path.join(settings.PROJECT_PATH,'../data/bonini/01_PIC_F5_09022012_mot_mirror_mrgSORTED.nex'),
+                                os.path.join(settings.PROJECT_PATH,'../data/bonini/02_Pic_F5_10022012_mot_mirror_mrgSORTED.nex')])

395f0a276247fdbf4bc207465af46e6961d78861
jbonaiuto <jbonaiuto@gmail.com>
2016-07-04 22:01:23 +0100

    refactoring

--- sensorimotordb/tests/api_test.py
+++ sensorimotordb/tests/api_test.py
@@ -1,6 +1,20 @@
+import sys
+import django
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "uscbp.settings")
 from tastypie.test import TestApiClient
 
 def test_api(username, password):
     api_client = TestApiClient()
     result = api_client.client.login(username=username, password=password)
-    print(api_client.get('http://localhost:8000/sensorimotordb/api/v1/experiment/57/', format='json'))
+    #print(api_client.get('http://localhost:8000/sensorimotordb/api/v1/visuomotor_classification_analysis_results/?analysis__experiment=73', format='json'))
+    print(api_client.get('http://localhost:8000/sensorimotordb/api/v1/visuomotor_classification_analysis_results/5/', format='json'))
+    #print(api_client.get('http://localhost:8000/sensorimotordb/api/v1/visuomotor_classification_analysis/1/', format='json'))
+    #print(api_client.get('http://localhost:8000/sensorimotordb/api/v1/analysis/1/', format='json'))
+    #print(api_client.get('http://localhost:8000/sensorimotordb/api/v1/unit_classification/1/', format='json'))
+
+
+if __name__=='__main__':
+    django.setup()
+    test_api(sys.argv[1],sys.argv[2])
